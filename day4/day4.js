af1630cd1c82cadb0192abe3a059de5d657a1188
rassassin <reecethandi11@gmail.com>
2026-01-03 19:17:31 +0000

    solved part 2

--- day4/day4.js
+++ day4/day4.js
@@ -34,13 +34,62 @@ function isRollAcessisble(input, row, col) {
   return true;
 }
 
-function solveDayFour(input) {
-  const partOne = findAccessibleRolls(input);
-
-  return { partOne };
+function parseInputInto2DArray(input) {
+  let twoDArray = [];
+  for (let i = 0; i < input.length; i++) {
+    twoDArray.push(input[i].split(""));
+  }
+  return twoDArray;
 }
 
-const n = 1000;
+function removeRollsAndFindNewAccessibleRolls(inputParsedInto2DArray) {
+  let count = 0;
+  let inputCopy = inputParsedInto2DArray.slice();
+  //   let linesCheckedTwice = [0];
+
+  for (let row = 0; row < inputCopy.length; row++) {
+    for (let col = 0; col < inputCopy[row].length; col++) {
+      if (inputCopy[row][col] != "@") continue;
+      let isAccessible = isRollAcessisbleAndRemoveable(inputCopy, row, col);
+
+      if (isAccessible) {
+        inputCopy[row].splice(col, 1, ".");
+        count++;
+        // if ((row % 3 === 0 && !linesCheckedTwice.includes(row)) || row == inputCopy.length - 1) {
+        //   linesCheckedTwice.push(row);
+        row = 0;
+        // }
+      }
+    }
+  }
+  return count;
+}
+
+function isRollAcessisbleAndRemoveable(input, row, col) {
+  let count = 0;
+  for (let dir = 0; dir < 8; dir++) {
+    let currX = row + xDir[dir];
+    let currY = col + yDir[dir];
+
+    if (currX >= input.length || currX < 0 || currY >= input[0].length || currY < 0) continue;
+
+    if (input[currX][currY] === "@") {
+      count++;
+      if (count === 4) return false;
+    }
+  }
+  return true;
+}
+
+function solveDayFour(input) {
+  const partOne = findAccessibleRolls(input);
+  const inputParsedInto2DArray = parseInputInto2DArray(input);
+  const partTwo = removeRollsAndFindNewAccessibleRolls(inputParsedInto2DArray);
+
+  return { partOne, partTwo };
+}
+
+const n = 1;
 const start = performance.now();
 for (let i = 0; i < n; i++) {
   solveDayFour(input);
