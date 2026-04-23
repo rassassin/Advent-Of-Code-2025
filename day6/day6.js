79410ad8d304c903a4872da3a6f2677d1127ee75
Reece Thandi <Reece.Thandi@mhr.co.uk>
2026-04-23 11:20:57 +0100

    Solve Part one

--- day6/day6.js
+++ day6/day6.js
@@ -1,22 +1,68 @@
 const fs = require("fs");
+const { parse } = require("path");
 const input = fs.readFileSync("./day6input.txt", "utf-8").split("\n");
 
 function parseInput(input) {
     let arrayOfNumberArrays = [];
+
     for (let i = 0; i < 4; i++) {
-        const lineOfNumbers = input[i].split(" ").filter(v => v).map(n => Number(n));
+        const lineOfNumbers = input[i].split(" ").filter(x => x).map(n => Number(n));
         arrayOfNumberArrays.push(lineOfNumbers);
     }
 
-    arrayOfNumberArrays.push(input[4].split(""))
+    arrayOfNumberArrays.push(input[4].split(" ").filter(x => x))
 
-    return arrayOfNumberArrays
-}
+    let verticalNumberArraysAndSymbols = [];
 
-function checkLengths(input) {
-    for (const lines of input) {
-        console.log(lines.length)
+    for (let i = 0; i < arrayOfNumberArrays[0].length; i++) {
+        let tempArr = [];
+        tempArr.push(arrayOfNumberArrays[0][i])
+        tempArr.push(arrayOfNumberArrays[1][i])
+        tempArr.push(arrayOfNumberArrays[2][i])
+        tempArr.push(arrayOfNumberArrays[3][i])
+        tempArr.push(arrayOfNumberArrays[4][i])
+
+        verticalNumberArraysAndSymbols.push(tempArr)
     }
+
+    return verticalNumberArraysAndSymbols;
 }
 
-console.log(checkLengths(input))
+function AddOrMutiliplyNumArrays(parsedInput) {
+    let total = 0;
+    
+    for(let i = 0; i < parsedInput.length; i++) {
+        if(parsedInput[i][4] === '*') {
+            total += parsedInput[i].slice(0, 4).reduce((acc, curr) => acc * curr)
+        } else {
+            total += parsedInput[i].slice(0, 4).reduce((acc, curr) => acc + curr)
+        }
+    }
+
+    return total
+}
+
+function daySix(input) {
+    const parsedInput = parseInput(input);
+    const solvePartOne = AddOrMutiliplyNumArrays(parsedInput)
+
+    return { solvePartOne }
+}
+
+function checkForMissingSymbol(verticalNumberArraysAndSymbols) {
+    for (const lines of verticalNumberArraysAndSymbols) {
+        console.log(lines)
+        if(!lines[4]) {
+        return lines
+        }
+    }
+
+    return "Okay"
+}
+
+const result = parseInput(input);
+
+
+// fs.writeFileSync('output.txt', JSON.stringify(result, null, 2));
+
+console.log(daySix(input))
