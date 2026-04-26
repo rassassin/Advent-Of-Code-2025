d4ba6ea4d812d313952fff30cbf5e548d9f9648e
rassassin <reecethandi11@gmail.com>
2026-04-26 18:53:38 +0100

    added loop

--- day6/day6.js
+++ day6/day6.js
@@ -2,81 +2,81 @@ const fs = require("fs");
 const input = fs.readFileSync("./day6input.txt", "utf-8").split("\n");
 
 function parseInput(input) {
-    let arrayOfNumberArrays = [];
+  let arrayOfNumberArrays = [];
 
-    for (let i = 0; i < 4; i++) {
-        const lineOfNumbers = input[i].split(" ").filter(x => x).map(n => Number(n));
-        arrayOfNumberArrays.push(lineOfNumbers);
+  for (let i = 0; i < 4; i++) {
+    const lineOfNumbers = input[i]
+      .split(" ")
+      .filter((x) => x)
+      .map((n) => Number(n));
+    arrayOfNumberArrays.push(lineOfNumbers);
+  }
+
+  arrayOfNumberArrays.push(input[4].split(" ").filter((x) => x));
+
+  let verticalNumberArraysAndSymbols = [];
+
+  for (let i = 0; i < arrayOfNumberArrays[0].length; i++) {
+    let tempArr = [];
+    for (let j = 0; j < 5; j++) {
+      tempArr.push(arrayOfNumberArrays[j][i]);
     }
 
-    arrayOfNumberArrays.push(input[4].split(" ").filter(x => x))
+    verticalNumberArraysAndSymbols.push(tempArr);
+  }
 
-    let verticalNumberArraysAndSymbols = [];
-
-    for (let i = 0; i < arrayOfNumberArrays[0].length; i++) {
-        let tempArr = [];
-        tempArr.push(arrayOfNumberArrays[0][i])
-        tempArr.push(arrayOfNumberArrays[1][i])
-        tempArr.push(arrayOfNumberArrays[2][i])
-        tempArr.push(arrayOfNumberArrays[3][i])
-        tempArr.push(arrayOfNumberArrays[4][i])
-
-        verticalNumberArraysAndSymbols.push(tempArr)
-    }
-
-    return verticalNumberArraysAndSymbols;
+  return verticalNumberArraysAndSymbols;
 }
 
 function parsedInputForPartTwo(input) {
-    let arrayOfNumberArrays = [];
+  let arrayOfNumberArrays = [];
 
-    for (let i = 0; i < 4; i++) {
-        const lineOfNumbers = input[i].split(" ");
-        arrayOfNumberArrays.push(lineOfNumbers);
-    }
+  for (let i = 0; i < 4; i++) {
+    const lineOfNumbers = input[i].split(" ");
+    arrayOfNumberArrays.push(lineOfNumbers);
+  }
 
-    arrayOfNumberArrays.push(input[4].split(" ").filter(x => x))
+  arrayOfNumberArrays.push(input[4].split(" ").filter((x) => x));
 
-    return arrayOfNumberArrays
+  return arrayOfNumberArrays;
 }
 
 function AddOrMutiliplyNumArrays(parsedInput) {
-    let total = 0;
-    
-    for(let i = 0; i < parsedInput.length; i++) {
-        if(parsedInput[i][4] === '*') {
-            total += parsedInput[i].slice(0, 4).reduce((acc, curr) => acc * curr)
-        } else {
-            total += parsedInput[i].slice(0, 4).reduce((acc, curr) => acc + curr)
-        }
-    }
+  let total = 0;
 
-    return total
+  for (let i = 0; i < parsedInput.length; i++) {
+    if (parsedInput[i][4] === "*") {
+      total += parsedInput[i].slice(0, 4).reduce((acc, curr) => acc * curr);
+    } else {
+      total += parsedInput[i].slice(0, 4).reduce((acc, curr) => acc + curr);
+    }
+  }
+
+  return total;
 }
 
 function daySix(input) {
-    const parsedInput = parseInput(input);
-    const solvePartOne = AddOrMutiliplyNumArrays(parsedInput)
+  const parsedInput = parseInput(input);
+  const solvePartOne = AddOrMutiliplyNumArrays(parsedInput);
 
-    return { solvePartOne }
+  return { solvePartOne };
 }
 
 function checkForMissingSymbol(verticalNumberArraysAndSymbols) {
-    for (const lines of verticalNumberArraysAndSymbols) {
-        console.log(lines)
-        if(!lines[4]) {
-        return lines
-        }
+  for (const lines of verticalNumberArraysAndSymbols) {
+    console.log(lines);
+    if (!lines[4]) {
+      return lines;
     }
+  }
 
-    return "Okay"
+  return "Okay";
 }
 
 const result = parseInput(input);
 
-
 // fs.writeFileSync('output.txt', JSON.stringify(result, null, 2));
 
-console.log(parsedInputForPartTwo(input))
+console.log(daySix(input));
 
 // console.log(daySix(input))
